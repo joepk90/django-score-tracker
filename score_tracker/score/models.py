616c89570c74cb79417b5e38a10e67853fea0c15
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2023-01-08 19:31:25 +0000

    setup score model

--- score_tracker/score/models.py
+++ score_tracker/score/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Score(models.Model):
+    number = models.PositiveSmallIntegerField(validators=[
+        MinValueValidator(1),
+        MaxValueValidator(10)
+    ])
+    date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+
+    # city = models.CharField(max_length=255) # TODO - extract from IP
+    # regio/n = models.CharField(max_length=255) # TODO - extract from IP
+    # country = models.CharField(max_length=255) # TODO - extract from IP
