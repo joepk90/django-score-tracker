4de87c32ac280f25d450dc1d4738b2655cebdb40
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2023-01-14 17:25:38 +0000

    disable score view set throttle logic

--- score_tracker/score/views.py
+++ score_tracker/score/views.py
@@ -23,12 +23,11 @@ class ScoreViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, Gener
     # https://www.pedaldrivenprogramming.com/2017/05/throttling-django-rest-framwork-viewsets/
     # https://stackoverflow.com/questions/36039843/django-rest-post-and-get-different-throttle-scopes
 
-    def get_throttles(self):
-        if self.action == 'create':
-            self.throttle_scope = 'scores.create'
-            self.throttle_classes = [ScopedRateThrottle]
-
-        return super().get_throttles()
+    # def get_throttles(self):
+    # if self.action == 'create':
+    # self.throttle_scope = 'scores.create'
+    # self.throttle_classes = [ScopedRateThrottle]
+    # return super().get_throttles()
 
     def get_serializer_class(self):
 
