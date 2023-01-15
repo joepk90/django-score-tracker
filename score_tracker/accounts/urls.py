7016b8570823733f4608d0d2ca4d38d25b8fc945
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2023-01-15 15:13:32 +0000

    setup djoser user endpoints - import re_path to handle auth url setting

--- score_tracker/accounts/urls.py
+++ score_tracker/accounts/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.urls import re_path
 from . views import CustomDjsoserUserViewSet
 
 router = DefaultRouter()
 router.register("users", CustomDjsoserUserViewSet)
 
+
 # TODO: enable djoser auth endpoints if authenticated as super user
 
+
 urlpatterns = [
-    path('', include('djoser.urls.jwt')),
+    re_path(r'^auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
 
 urlpatterns += router.urls
