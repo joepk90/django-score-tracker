d2351742686846c0e032e8cf6b4a926744a50440
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2023-01-15 21:26:30 +0000

    fix but in accounts urls - some urls not accessible

--- score_tracker/accounts/urls.py
+++ score_tracker/accounts/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from django.urls import re_path
 from . views import CustomDjsoserUserViewSet
 from . views import GuestUserUpdateViewSet
 
@@ -9,12 +8,8 @@ router.register("guest", GuestUserUpdateViewSet)
 router.register("users", CustomDjsoserUserViewSet)
 
 
-# TODO: enable djoser auth endpoints if authenticated as super user
-
-
 urlpatterns = [
-    re_path(r'^auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('', include('djoser.urls.jwt')),
 ]
 
 urlpatterns += router.urls
