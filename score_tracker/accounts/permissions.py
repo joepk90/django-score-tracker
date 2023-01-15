18941624a008517de2e8ea661661a890e6a131df
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2023-01-15 17:57:29 +0000

    improve accounts IsAuthenticatedAndIsObjectOwner logic. ensure user is authenticated

--- score_tracker/accounts/permissions.py
+++ score_tracker/accounts/permissions.py
@@ -4,6 +4,14 @@ from score_tracker.exceptions import CustomAPIException
 from rest_framework import status
 
 
+def unauthorized_request():
+    message = 'Permission denied: Unauthorized'
+    raise CustomAPIException(
+        detail=message,
+        code=status.HTTP_401_UNAUTHORIZED
+    )
+
+
 class IsAuthenticatedAndIsObjectOwner(BasePermission):
 
     """
@@ -11,6 +19,10 @@ class IsAuthenticatedAndIsObjectOwner(BasePermission):
     """
 
     def has_object_permission(self, request, view,  obj):
+        user = request.user
+        if user.is_authenticated == False:
+            unauthorized_request()
+
         return request.user == obj.user
 
 
@@ -23,11 +35,7 @@ class IsGuest(BasePermission):
     def has_permission(self, request, view):
         user = request.user
         if user.is_authenticated == False:
-            message = 'Permission denied: Unauthorized'
-            raise CustomAPIException(
-                detail=message,
-                code=status.HTTP_401_UNAUTHORIZED
-            )
+            unauthorized_request()
 
         if user.is_guest != True:
             message = 'Cannot update credentials for non guest accounts'
