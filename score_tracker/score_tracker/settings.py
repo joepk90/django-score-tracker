89dc5cad2e347c9577a1a586ba7c8434a8749564
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2023-02-19 19:32:30 +0000

    update settings ALLOWED_HOSTS env var config

--- score_tracker/score_tracker/settings.py
+++ score_tracker/score_tracker/settings.py
@@ -35,11 +35,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-allowed_hosts_arr = [env("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = []
+ALLOWED_HOSTS_ENV_VAR = os.environ.get("ALLOWED_HOSTS", None)
 if (ENVIRONMENT == 'DEV'):
-    allowed_hosts_arr = ['*']
-
-ALLOWED_HOSTS = allowed_hosts_arr
+    ALLOWED_HOSTS = ['*']
+elif ALLOWED_HOSTS_ENV_VAR is not None:
+    ALLOWED_HOSTS = [ALLOWED_HOSTS_ENV_VAR]
 
 
 # Application definition
